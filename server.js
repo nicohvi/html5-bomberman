27a7da1ecb8fff45aca164c95b2f850cd25f1642
Nicolay Hvidsten <nico.hvi@gmail.com>
2015-04-30 12:03:13 +0200

    updated paths for new location

--- server.js
+++ server.js
@@ -5,17 +5,17 @@ const express = require("express");
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io').listen(server);
-const lib = require('./game/lib/lib');
+const lib = require('./server/game/lib/lib');
 const EventEmitter = require('events').EventEmitter;
 
 // Monkey patching/extensions 
 lib.extend(EventEmitter);
-const Server = require("./game/server");
+const Server = require("./server/game/server");
 
 Server.init(io);
 app.use(express.bodyParser());
 
 // Game
-app.use(express.static(__dirname + "/../public/"));
+app.use(express.static(__dirname + "/public/"));
 
 server.listen(8080);
